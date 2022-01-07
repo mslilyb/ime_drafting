81ff8bd3ebc9f8537c5f091bc6ff651dc53aadf2
mslilyb <lilith.r.m.d@gmail.com>
2022-01-07 03:34:35 -0800

    final commit before bed. Still to do is better score output

--- imelib.py
+++ imelib.py
@@ -3,7 +3,13 @@ import statistics
 import sys
 import itertools as iter
 
-
+def generatefilters(): #generates a list of legal kmers. in the future, user specified exceptions could be handled
+    filter = {}
+    perms = list(iter.product('ACTG', repeat=5))
+    for i in range(0, len(perms)):
+        perms[i] = ''.join(perms[i])
+        filter[perms[i]] = 1
+    return(filter)
 #expressionsplit():
     #return(Nothing)
 
@@ -21,6 +27,7 @@ def cutoffsplit(filename, cutoff):
                 prox.append(seq)
             else:
                 dist.append(seq)
+    fp.close()
     return(prox, dist)
 
 def kmercount(seqs, k): #determines the total count of kmers in the sequences
@@ -29,48 +36,55 @@ def kmercount(seqs, k): #determines the total count of kmers in the sequences
     total = 0
     decay = 1
     count = {}
-    filter = list(iter.product('ACTG', repeat=5))
-    for i in range(0, len(filter)):
-            filter[i] = ''.join(filter[i])
-    #print(filter)
+    filter = generatefilters() #a dictionary of all the possible legal kmers. reformatting to dictionary drastically cuts down processing time
+
     for s in seqs:
         for i in range(don, len(s)-k+1-acc):
             kmer = s[i:i+k]
-            if kmer in filter:
+            if kmer in filter: #this is a CPU sink, really big O
                 if kmer not in count:
                     count[kmer] = 0
                 count[kmer] += decay #to factor in geom decay, to account for intron significance dropping off as it lengthens
                 total += decay #pretty sure we want to make the total with the decay as well
-            #some sort of decay function...
-            if (decay):
-                """
-                can't be flat...can it? every 50 bases you turn it down by like 0.1? because
-                depending on how long or short the intron is you won't account for the start vs. end consistently
-                so maybe we take the whole length and every increment we decrement the decay. This makes it
-                consistent across introns regardless of length.
+                #some sort of decay function goes here...
 
-                In order to do this, I think I need a cutoff, like where in the intron the significance drops
-                off. May have to be experimentally determined...but if it's a cutoff, a flat value may make more
-                sense. Does a 1000bp intron have a larger "range of significance" than a 100bp one? Good to ask!
-
-                decay function draft, flat decrement value
-                #lets assume without cause that anything after 100 bp is less important...by some...proportion...
-                sig_assumed = 100
-                rate_assumed = 0.5
-                if i+k <= sig_assumed -1:
-                    decay *= rate_assumed
-                    sig_assumed += sig_assumed
-                #okay...now lets say instead the decay is more based on how far on any given intron you've gone down.
-                #The assumptions here are again a suitable rate of decay and how often you want to decrement.
-                rate_assumed = 0.5
-                proportion_assumed = 0.1
-                sig_interval = len(s) * proportion_assumed
-                #then again, the same idea
-                if i+k <= sig_interval -1:
-                    decay *= rate_assumed
-                    sig_interval += sig_interval
-                """
     return(count, total) #returns a tuple, a dictionary keyed with unique kmers and their counts, and the total number)
 
 def kmerfreqs(counts): #determines frequencies of kmers across an introns
     #print(counts)
+    freqs= {}
+    for kmer in counts[0]:
+        freqs[kmer] = counts[0][kmer] / counts[1]
+    return(freqs)
+
+def training(proxfreqs, distalfreqs, xfold): #calculates the log odd probability for our kmerfreqs
+    trained = {}
+    for kmer in sorted(proxfreqs):
+        trained[kmer] = math.log2(proxfreqs[kmer] / distalfreqs[kmer])
+    return(trained)
+
+def scoring(prox, distal, trained, k):
+    proxscores = []
+    distalscores = []
+    don = 5
+    acc = 10
+
+    for seq in prox:
+        score = 0
+        for i in range(don, len(seq) -k +1 -acc):
+            kmer = seq[i:i+k]
+            if kmer in trained:
+                score += trained[kmer]
+        proxscores.append(score)
+
+    for seq in distal:
+        score = 0
+        for i in range(don, len(seq) -k +1 -acc):
+            kmer = seq[i:i+k]
+            if kmer in trained:
+                score += trained[kmer]
+        distalscores.append(score)
+
+    print(statistics.mean(proxscores))
+    print(statistics.mean(distalscores))
+    #print(proxscores)
